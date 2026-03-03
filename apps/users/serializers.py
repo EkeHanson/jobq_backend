09c8c5a94a823b9eef91b5eeed2b01d2b7be9066
Ekene-onwon Abraham <ekenehanson@gmail.com>
2026-03-03 13:45:50 -0800

    first commit

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -9,6 +9,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'username': {'required': False},
+            'email': {'required': False},
+        }
 
 
 class RegisterSerializer(serializers.ModelSerializer):
